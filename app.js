c9f19b2206ff10fc3f0564bd0d67967ee0175f88
Khayal <khayalmans994@gmail.com>
2023-12-02 14:43:25 +0400

    project1

---
+++ app.js
@@ -0,0 +1,16 @@
+let numberOffilms = prompt  ("Neçə kinoya baxmısınız?") 
+
+
+const personalMovieDB = {
+    count : netice,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+}
+
+let film = prompt ("Axrıncı baxdığınız film ?")
+let bal = prompt ("Neçə bal ilə qiymətləndirirsiniz ?")
+
+personalMovieDB.movies[film] = bal;
+console.log(personalMovieDB);
